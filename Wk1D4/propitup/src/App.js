5776a3ee5bd7094ce1d445571228f04bb8af47f9
John Cartwright <mrjc3@outlook.com>
2022-09-02 12:42:17 -0700

    added new functionality

--- Wk1D4/propitup/src/App.js
+++ Wk1D4/propitup/src/App.js
@@ -2,14 +2,22 @@ import logo from './logo.svg';
 import './App.css';
 import PersonCard from './components/PersonCard.js';
 
+var peopleArr =[
+  {"firstName":"Jane", "lastName":"Doe", "age":45, "hairColor":"Black"},
+  {"firstName":"John", "lastName":"Smith","age":88,"hairColor":"Brown"},
+  {"firstName":"Millard", "lastName":"Fillmore","age":50,"hairColor":"Brown"},
+  {"firstName":"Maria", "lastName":"Smith","age":62,"hairColor":"Brown"}
+]
+
 
 function App() {
   return (
     <div className="App">
-     <PersonCard firstName={"Jane"} lastName={"Doe"} age={45} hairColor={"Black"}/>
-     <PersonCard firstName={"John"} lastName={"Smith"} age={88} hairColor={"Black"}/>
-     <PersonCard firstName={"Millard"} lastName={"Fillmore"} age={45} hairColor={"Black"}/>
-     <PersonCard firstName={"Maria"} lastName={"Smith"} age={45} hairColor={"Black"}/>
+      {peopleArr.map(person => {
+        return <PersonCard firstName={person.firstName} lastName={person.lastName}
+        age={person.age} hairColor={person.hairColor} />
+      })
+      }
     </div>
   );
 }
--- Wk1D4/propitup/src/components/PersonCard.js
+++ Wk1D4/propitup/src/components/PersonCard.js
@@ -4,15 +4,26 @@ class PersonCard extends Component {
 
     constructor(props) {
         super(props);
+
+        this.state = {
+            age: this.props.age,
+        };
+    }
+    
+    incrementAge = () => {
+        const {age} = this.state;
+        this.setState({age: age + 1});
     }
 
     render() {
-        const {firstName, lastName, age, hairColor} = this.props;
+        const {firstName, lastName, hairColor} = this.props;
+        const { age } = this.state;
         return (
             <div>
                 <h1>{lastName}, {firstName}</h1>
                 <h4>Age: {age}</h4>
                 <h4>Hair Color: {hairColor}</h4>
+                <button onClick={this.incrementAge}>Birthday Button for {firstName} {lastName}</button>
             </div>
         )
     }
